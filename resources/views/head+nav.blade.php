c72d01b8d66eb383240e1ec9d52fe6307eb06677
titanggraita <titanggraita@gmail.com>
2021-01-22 09:33:31 +0700

    copy code to clipboard

--- resources/views/head+nav.blade.php
+++ resources/views/head+nav.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="datepicker/css/datepicker.css">
 
     <script src="/jquery/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/clipboard.js/1.5.16/clipboard.min.js"></script>
     <script src="datepicker/js/bootstrap-datepicker.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.0/moment.min.js"></script>
     <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
