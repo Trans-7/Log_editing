2d86a22fb6856a01efe3f1cb685d4d37ceed783c
titanggraita <titanggraita@gmail.com>
2021-02-01 09:42:50 +0700

    update lagi

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -112,68 +112,71 @@
                 data:{from_date:from_date, to_date:to_date, nik:nik, name:name, program:program, kerja:kerja, _token:_token},
                 dataType:"json",
                 success:function(data){
-                    var output = '';
-                    $('#total_records').text(data.length);
-                    for(var count=0; count<data.length;count++){
-                        output += '<tr>';
-                        output += '<td>' + data[count].logeditingreport_editor_nik + '</td>';
-                        output += '<td>' + data[count].logeditingreport_editor_name + '</td>';
-                        output += '<td>' + data[count].logeditingreport_editor_email + '</td>';
-                        output += '<td>' + data[count].logeditingreport_systemkerja + '</td>';
-                        output += '<td>' + moment(data[count].logeditingreport_date).format('YYYY-MM-DD');+ '</td>';
-                        output += '<td>' + data[count].logeditingreport_shift + '</td>';
-                        output += '<td>' + data[count].logeditingreport_program + '</td>';
-                        output += '<td>' + data[count].logeditingreport_episode + '</td>';
-                        output += '<td>' + data[count].logeditingreport_segment + '</td>';
-                        output += '<td>' + 
-                        '<button type="button" class="btn btn-blue btn-sm" data-toggle="modal" data-target="#modalDetail-' + data[count].logeditingreport_id + '"> View Detail </button>' 
-                        + '<div class="modal fade bd-example-modal-lg" id="modalDetail-' + data[count].logeditingreport_id + '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">'
-                        + '<div class="modal-dialog modal-lg" role="document">'
-                        + '<div class="modal-content">'
-                        + '<div class="modal-header">'
-                        + '<h3 class="modal-title" id="exampleModalLabel" style="color:#1b215a;">Detail Report</h3>'
-                        + '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
-                        + '</div>'
-                        + '<div class="modal-body">'
-                        + '<div class="panel-body">' 
-                        + '<div class="table-responsive">'
-                        + '<table class="table table-striped table-bordered">'
-                        + '<thead class="table-head text-center">'
-                        + '<tr><th>Code</th><th>User Pendamping</th><th>Request ID</th><th>Remark</th><th>Booth</th><th>Alasan (WFO)</th><th>Alat (WFH)</th><th>Provider (WFH)</th><th>Downloadspeed (WFH)</th><th>Quotausage (WFH)</th><th>Screenshoot (WFH)</th><th>Remark (WFH)</th><th>Copy Size</th><th>Copy Segment</th><th>Copy Date</th><th>Copy Remark</th>'
-                        + '<tr></thead>'
-                        + '<tbody class="table-body text-center"><td>' + data[count].logediting_code + '</td>'
-                        + '<td>' + data[count].logeditingreport_userpendamping + '</td>'
-                        + '<td>' + data[count].logeditingreport_requestid + '</td>'
-                        + '<td>' + data[count].logeditingreport_remark + '</td>'
-                        + '<td>' + data[count].logeditingreport_booth + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfo_alasan + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfh_alat + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfh_provider + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfh_downloadspeed + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfh_quotausage + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfh_screenshoot + '</td>'
-                        + '<td>' + data[count].logeditingreport_wfh_remark + '</td>'
-                        + '<td>' + data[count].logeditingreport_copy_size + '</td>'
-                        + '<td>' + data[count].logeditingreport_copy_segment + '</td>'
-                        + '<td>' + data[count].logeditingreport_copy_date+ '</td>'
-                        + '<td>' + data[count].logeditingreport_copy_remark + '</td>'
-                        + '</tbody>'
-                        + '</table>' 
-                        + '</div>' 
-                        + '</div>'
-                        + '<div class="modal-footer"><button type="button" class="btn btn-blue btn-md" data-dismiss="modal">Close</button></div>'
-                        + '</div>'
-                        + '</div>'
-                        + '</div>'
-                        + '</div>'
-                        + '</td>';
-                        output += '</tr>';
+                    if(data != 'NULL'){
+                        var output = '';
+                        $('#total_records').text(data.length);
+                        for(var count=0; count<data.length;count++){
+                            output += '<tr>';
+                            output += '<td>' + data[count].logeditingreport_editor_nik + '</td>';
+                            output += '<td>' + data[count].logeditingreport_editor_name + '</td>';
+                            output += '<td>' + data[count].logeditingreport_editor_email + '</td>';
+                            output += '<td>' + data[count].logeditingreport_systemkerja + '</td>';
+                            output += '<td>' + moment(data[count].logeditingreport_date).format('YYYY-MM-DD');+ '</td>';
+                            output += '<td>' + data[count].logeditingreport_shift + '</td>';
+                            output += '<td>' + data[count].logeditingreport_program + '</td>';
+                            output += '<td>' + data[count].logeditingreport_episode + '</td>';
+                            output += '<td>' + data[count].logeditingreport_segment + '</td>';
+                            output += '<td>' + 
+                            '<button type="button" class="btn btn-blue btn-sm" data-toggle="modal" data-target="#modalDetail-' + data[count].logeditingreport_id + '"> View Detail </button>' 
+                            + '<div class="modal fade bd-example-modal-lg" id="modalDetail-' + data[count].logeditingreport_id + '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">'
+                            + '<div class="modal-dialog modal-lg" role="document">'
+                            + '<div class="modal-content">'
+                            + '<div class="modal-header">'
+                            + '<h3 class="modal-title" id="exampleModalLabel" style="color:#1b215a;">Detail Report</h3>'
+                            + '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+                            + '</div>'
+                            + '<div class="modal-body">'
+                            + '<div class="panel-body">' 
+                            + '<div class="table-responsive">'
+                            + '<table class="table table-striped table-bordered">'
+                            + '<thead class="table-head text-center">'
+                            + '<tr><th>Code</th><th>User Pendamping</th><th>Request ID</th><th>Remark</th><th>Booth</th><th>Alasan (WFO)</th><th>Alat (WFH)</th><th>Provider (WFH)</th><th>Downloadspeed (WFH)</th><th>Quotausage (WFH)</th><th>Screenshoot (WFH)</th><th>Remark (WFH)</th><th>Copy Size</th><th>Copy Segment</th><th>Copy Date</th><th>Copy Remark</th>'
+                            + '<tr></thead>'
+                            + '<tbody class="table-body text-center"><td>' + data[count].logediting_code + '</td>'
+                            + '<td>' + data[count].logeditingreport_userpendamping + '</td>'
+                            + '<td>' + data[count].logeditingreport_requestid + '</td>'
+                            + '<td>' + data[count].logeditingreport_remark + '</td>'
+                            + '<td>' + data[count].logeditingreport_booth + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfo_alasan + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfh_alat + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfh_provider + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfh_downloadspeed + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfh_quotausage + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfh_screenshoot + '</td>'
+                            + '<td>' + data[count].logeditingreport_wfh_remark + '</td>'
+                            + '<td>' + data[count].logeditingreport_copy_size + '</td>'
+                            + '<td>' + data[count].logeditingreport_copy_segment + '</td>'
+                            + '<td>' + data[count].logeditingreport_copy_date+ '</td>'
+                            + '<td>' + data[count].logeditingreport_copy_remark + '</td>'
+                            + '</tbody>'
+                            + '</table>' 
+                            + '</div>' 
+                            + '</div>'
+                            + '<div class="modal-footer"><button type="button" class="btn btn-blue btn-md" data-dismiss="modal">Close</button></div>'
+                            + '</div>'
+                            + '</div>'
+                            + '</div>'
+                            + '</div>'
+                            + '</td>';
+                            output += '</tr>';
+                        }
                     }
                     $('tbody').html(output);
                 }
             });
         }
         $('#filter').click(function(){
+            var output = output;
             var from_date = $('#from_date').val();
             var to_date = $('#to_date').val();
             var nik = $('#nik').val();
@@ -182,10 +185,10 @@
             var kerja = $('#kerja').val();
 
             if(from_date != '' && to_date != '' || nik != '' || name != '' || program != '' || kerja != ''){
-                fetch_data(from_date, to_date, nik, name, program, kerja)
+                fetch_data(from_date, to_date, nik, name, program, kerja);
             }
             else{
-                alert('Error');
+                fetch_data(from_date, to_date, nik, name, program, kerja);
             }
         });
     });
